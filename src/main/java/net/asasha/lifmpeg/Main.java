3ee8919f617fb97b795dc9e1811e35a9dfc30f61
Alex <sumy.ua@gmail.com>
2017-10-17 11:01:41 +0300

    My FFmpeg project (MVC) Refresh version

--- src/main/java/net/asasha/lifmpeg/Main.java
+++ src/main/java/net/asasha/lifmpeg/Main.java
@@ -3,9 +3,9 @@ package net.asasha.lifmpeg;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-            System.out.println("Starting FFmpeg!");
-            for (String arg : args)
-                System.out.println(arg);
+        // write your code here
+        System.out.println("Starting FFmpeg!");
+        for (String arg : args)
+            System.out.println(arg);
     }
 }
