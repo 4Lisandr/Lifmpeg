0c122378fd8464c0b1214242c2bb81f187aa9df3
Alex <sumy.ua@gmail.com>
2018-03-23 13:10:47 +0200

    Added execute method

--- src/main/java/net/asasha/lifmpeg/model/FfmpegManager.java
+++ src/main/java/net/asasha/lifmpeg/model/FfmpegManager.java
@@ -13,29 +13,29 @@ public class FfmpegManager implements VideoManager {
     public void concat(String in, String out) {
 
         String command = "cmd /c start /min ffmpeg -y -f concat -i \"%s\" -c copy \"%s\"";
-        String fullCommand = String.format(command, in, out);
-
-        try {
-            Runtime.getRuntime().exec(fullCommand);
-        }
-        catch (Exception e){e.printStackTrace();}
+        execute(command, in, out);
     }
 
+
     @Override
-    public void createImage(String in, String time, String out) {
+    public void createAnImage(String in, String time, String out) {
         //  ffmpeg -i input.flv -ss 00:00:14.435 -vframes 1 out.png
 
         String command = "cmd /c start /min ffmpeg -i \"%s\" -ss \"%s\" -vframes 1 \"%s\"";
-        String fullCommand = String.format(command, in, time, out);
+        execute(command, in, time, out);
+    }
+
+    private void createImage(String input, double sec, String output) throws IOException {
+        createAnImage(input, timeFormatter(sec), output);
+    }
+
+    private static void execute(String command, String ... options) {
+        String fullCommand = String.format(command, (Object[]) options);
 
         try {
             Runtime.getRuntime().exec(fullCommand);
         }
         catch (Exception e){e.printStackTrace();}
-    }
-
-    private void createImage(String input, double sec, String output) throws IOException {
-        createImage(input, timeFormatter(sec), output);
     }
 
     private static String timeFormatter(double durable){
